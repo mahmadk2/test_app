975525174517a7e122365fe5b2168dee16f284a3
ahmad <mahmadk2@hotmail.com>
2016-04-13 16:25:19 +0500

    update the edit, new, index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,18 @@
 class ArticlesController < ApplicationController
 	
+	
+	
+	def index
+		@articles = Article.all
+	end
+	
 	def new 
 		@article = Article.new
 		
+	end
+	
+	def edit
+		@article = Article.find(params[:id])
 	end
 	
 	def create
@@ -19,9 +29,24 @@ class ArticlesController < ApplicationController
 		
 	end
 	
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice] = "THAT IS Succesfully UPDATED"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
+			
+			
+	end
+	
 	def show
 		@article = Article.find(params[:id])
 	end
+	
+	
+	
 	
 	private
 	
@@ -29,5 +54,7 @@ class ArticlesController < ApplicationController
 		params.require(:article).permit(:title, :description)
 	end
 	
+	end
 	
-end
+	
+	
