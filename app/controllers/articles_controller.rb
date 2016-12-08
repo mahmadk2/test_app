250cef8202e74a1f61e78bf096c49dfb861adda3
ahmad <mahmadk2@hotmail.com>
2016-12-08 19:08:49 +0500

    debbuger

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,8 +16,10 @@ class ArticlesController < ApplicationController
 	end
 	
 	def create
+		debugger
 		
 		@article = Article.new(article_params)
+		@article.user = User.first
 		
 		if @article.save
 			flash[:success] = "Article succesfully created"
